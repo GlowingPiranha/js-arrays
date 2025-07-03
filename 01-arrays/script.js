a8facde8ec1e94911de72ef58378a41dd602103f
GlowingPiranha <ikimus01@gmail.com>
2025-07-03 16:32:07 +0200

    [apt] 2-Patrick

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -15,6 +15,10 @@ console.log(fourthTeacher)
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
+const patrick = `Patrick`
+teachers[4] = patrick
+console.log(teachers)
+
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
 

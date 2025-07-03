a859efedcedfe14d8a112bf1f6a70f6be5e4439a
GlowingPiranha <ikimus01@gmail.com>
2025-07-03 17:01:31 +0200

    [apt] 6-Sarah

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -38,6 +38,8 @@ teachers.splice(6, 0, `Vanessa`)
 console.log(teachers)
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.splice(0, 0, `Sarah`)
+console.log(teachers)
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
